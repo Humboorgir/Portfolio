3385379d61052fbf0e4fdc0f701e2239cb5141c5
Humboorgir <hardcraftap@gmail.com>
2023-07-17 00:24:36 +0330

    Import and use particles component in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/global/navbar";
+import Particles from "@/components/global/particles";
 import Hero from "@/components/home/hero";
 
 export default function Home() {
   return (
     <>
       <section className="flex flex-col items-center min-h-screen">
+        <Particles />
         <Navbar />
         <Hero />
       </section>

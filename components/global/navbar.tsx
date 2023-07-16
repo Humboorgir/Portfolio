aaf7ac049e738f1fdd9bd0dbca4029756add0bf8
Humboorgir <hardcraftap@gmail.com>
2023-07-16 14:17:59 +0330

    Display logo on navbar

--- components/global/navbar.tsx
+++ components/global/navbar.tsx
@@ -1,5 +1,13 @@
 const Navbar = () => {
-  return <nav className="w-full"></nav>;
+  return (
+    <nav className="w-full flex items-center justify-between p-5 md:px-[7%]">
+      <h3 className="text-2xl">
+        <span className="text-violet-600">{"<"}</span>
+        <span className="text-sky-500">{"Iliya"}</span>
+        <span className="text-violet-600">{"/>"}</span>
+      </h3>
+    </nav>
+  );
 };
 
 export default Navbar;

f2b123305332d613005c5fdc3b949319a6bf0628
Humboorgir <hardcraftap@gmail.com>
2023-07-17 23:41:15 +0330

    Create projects component

--- components/home/projects/projects.tsx
+++ components/home/projects/projects.tsx
@@ -1,5 +1,23 @@
-const Projects = () => {
-  return <div>Projects</div>;
+import Project from "@/components/home/projects/project";
+
+interface Project {
+  title: string;
+  description: string;
+  image: string;
+}
+
+interface Props {
+  projects: Project[];
+}
+
+const Projects = ({ projects }: Props) => {
+  return (
+    <div className="flex flex-wrap items-center gap-4">
+      {projects.map((project) => {
+        return <Project project={project} />;
+      })}
+    </div>
+  );
 };
 
 export default Projects;

d0cc40de8eee8ae0965104304a205972ebf5ea4d
Humboorgir <hardcraftap@gmail.com>
2023-07-17 23:08:22 +0330

    Display personal info below technologies and description

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,16 +40,36 @@ export default function Home() {
 
       {/* about section  */}
       <section className="flex min-h-screen flex-col items-center pt-16">
+        {/* description and technologies */}
         <div
-          className="relative flex flex-col before:absolute before:left-[50%] before:top-[50%] before:-z-10 before:h-[calc(100%+50px)] before:w-[calc(100%+50px)] before:translate-x-[-50%] 
-      before:translate-y-[-50%] before:rounded-3xl before:bg-gray-800 before:blur-[120px] before:content-['']
-       md:flex-row md:gap-12 "
+          className="relative mb-12 flex flex-col before:absolute before:left-[50%] before:top-[50%] before:-z-10
+           before:h-[calc(100%+50px)] before:w-[calc(100%+50px)] before:translate-x-[-50%] before:translate-y-[-50%] before:rounded-3xl
+            before:bg-gray-800 before:blur-[120px] before:content-[''] md:flex-row md:gap-12"
         >
           <div>
             <AboutTitle />
             <AboutDescription />
           </div>
           <AboutTechnologies />
+        </div>
+
+        <div className="flex w-full max-w-4xl flex-wrap justify-around">
+          <div className="flex flex-col">
+            <span className="text-slate-300">Name:</span>
+            <span className="text-lg">Iliya</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-slate-300">Date of birth:</span>
+            <span className="text-lg">2008/5/9</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-slate-300">Experience:</span>
+            <span className="text-lg">+3 years</span>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-slate-300">Location:</span>
+            <span className="text-lg">Iran</span>
+          </div>
         </div>
       </section>
     </>

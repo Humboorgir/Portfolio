fe8ae6b2dda0bcf8964c3c8d925d5274739fea4f
Humboorgir <hardcraftap@gmail.com>
2023-07-18 00:36:37 +0330

    Modify <Iliya/>'s color (in navbar)

--- components/global/navbar.tsx
+++ components/global/navbar.tsx
@@ -1,10 +1,10 @@
 const Navbar = () => {
   return (
-    <nav className="w-full flex items-center justify-between p-5 md:px-[7%]">
+    <nav className="flex w-full items-center justify-between p-5 md:px-[7%]">
       <h3 className="text-2xl">
-        <span className="text-violet-600">{"<"}</span>
-        <span className="text-sky-500">{"Iliya"}</span>
-        <span className="text-violet-600">{"/>"}</span>
+        <span className="text-violet-400">{"<"}</span>
+        <span className="text-sky-400">{"Iliya"}</span>
+        <span className="text-violet-400">{"/>"}</span>
       </h3>
 
       <ul className="flex items-center gap-8">
@@ -22,7 +22,9 @@ const Navbar = () => {
         </li>
       </ul>
 
-      <button className="py-2 px-3 bg-sky-600 rounded-md hover:cursor-pointer">Change Theme</button>
+      <button className="rounded-md bg-sky-600 px-3 py-2 hover:cursor-pointer">
+        Change Theme
+      </button>
     </nav>
   );
 };

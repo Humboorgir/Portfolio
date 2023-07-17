bce689c792a89be753013832a75cbf6ee5a3499c
Humboorgir <hardcraftap@gmail.com>
2023-07-17 17:26:03 +0330

    Move hero section from its dedicated component to pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,40 @@
 import Navbar from "@/components/global/navbar";
 import Particles from "@/components/global/particles";
-import Hero from "@/components/home/hero";
+import HeroTitle from "@/components/home/hero/title";
+import HeroDescription from "@/components/home/hero/description";
+import HeroAboutButton from "@/components/home/hero/aboutbutton";
+import HeroContactButton from "@/components/home/hero/contactbutton";
+import HeroProgrammerImage from "@/components/home/hero/programmerimage";
 
 import { BsArrowDown } from "react-icons/bs";
 
 export default function Home() {
   return (
     <>
+      {/* hero section  */}
       <section className="relative flex min-h-screen flex-col items-center">
         <Particles />
         <Navbar />
-        <Hero />
+        <div className="mt-16 flex items-center justify-center">
+          <div
+            className="relative before:absolute before:left-[50%] before:top-[50%] before:-z-10 before:h-64 
+      before:w-[600px] before:translate-x-[-50%] before:translate-y-[-50%] before:rounded-full before:bg-gray-800
+       before:blur-[120px] before:content-['']"
+          >
+            <HeroTitle />
+            <HeroDescription />
+            <div className="flex items-center gap-3">
+              <HeroAboutButton />
+              <HeroContactButton />
+            </div>
+          </div>
+
+          <HeroProgrammerImage />
+        </div>
         <BsArrowDown className="mb-10 mt-auto h-16 w-16 animate-bounce opacity-50" />
       </section>
+
+      {/* about section  */}
       <section className="flex min-h-screen flex-col items-center"></section>
     </>
   );

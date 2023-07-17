024160fba40fbb89434269d591139a91713d2842
Humboorgir <hardcraftap@gmail.com>
2023-07-17 23:37:01 +0330

    Add margin-bottom to /projects/title.tsx

--- components/home/projects/title.tsx
+++ components/home/projects/title.tsx
@@ -1,5 +1,7 @@
 const Title = () => {
-  return <h2 className="text-3xl font-bold">Projects I've made previously</h2>;
+  return (
+    <h2 className="mb-6 text-3xl font-bold">Projects I've made previously</h2>
+  );
 };
 
 export default Title;

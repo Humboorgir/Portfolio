6d23ede40e32b5926e2e70144f48867e182cc6ac
Humboorgir <hardcraftap@gmail.com>
2023-07-18 23:48:01 +0330

    Use framer motion to animate global/link component

--- components/global/link.tsx
+++ components/global/link.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 
+import { motion } from "framer-motion";
 import Link from "next/link";
 interface Props {
   variant: "blue" | "violet";
@@ -15,13 +16,19 @@ const Button = ({ variant, href, children, className }: Props) => {
       ? "bg-gradient-to-br from-primary to-sky-800"
       : "bg-gradient-to-br from-violet-600 to-violet-700";
   return (
-    <Link
-      className={`flex w-fit scale-100 items-center justify-between gap-2 rounded-lg px-4 py-3
-       transition-transform hover:scale-105 active:scale-95 ${bgStyle} ${className}`}
-      href={href}
+    <motion.div
+      initial={{ scale: 1 }}
+      whileHover={{ scale: 1.02 }}
+      whileTap={{ scale: 0.98 }}
     >
-      {children}
-    </Link>
+      <Link
+        className={`flex w-fit scale-100 items-center justify-between gap-2 rounded-lg px-4 py-3
+       transition-transform hover:scale-105 active:scale-95 ${bgStyle} ${className}`}
+        href={href}
+      >
+        {children}
+      </Link>
+    </motion.div>
   );
 };
 

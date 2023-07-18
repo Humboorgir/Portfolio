021761bd09fee0d70088015279ba07fd4d5eb390
Humboorgir <hardcraftap@gmail.com>
2023-07-18 23:41:14 +0330

    Install framer-motion

--- components/global/link.tsx
+++ components/global/link.tsx
@@ -16,7 +16,8 @@ const Button = ({ variant, href, children, className }: Props) => {
       : "bg-gradient-to-br from-violet-600 to-violet-700";
   return (
     <Link
-      className={`flex w-fit items-center justify-between gap-2 rounded-lg px-4 py-3 ${bgStyle} ${className}`}
+      className={`flex w-fit scale-100 items-center justify-between gap-2 rounded-lg px-4 py-3
+       transition-transform hover:scale-105 active:scale-95 ${bgStyle} ${className}`}
       href={href}
     >
       {children}

d1e73136ebaf4bc9939fb985a4d076d5d66412f2
Humboorgir <hardcraftap@gmail.com>
2023-07-20 15:10:57 +0330

    Add scroll reveal animations to about/personalinfo component

--- components/home/about/personalinfo.tsx
+++ components/home/about/personalinfo.tsx
@@ -1,25 +1,47 @@
+import { motion } from "framer-motion";
+
 const PersonalInfo = () => {
   return (
     <div className="mt-20 grid w-full  max-w-md grid-cols-2 place-items-center gap-8 md:mt-0 md:max-w-4xl md:grid-cols-4">
-      <div className="flex flex-col">
+      <motion.div
+        initial={{ opacity: 0, x: "100px" }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+        className="flex flex-col"
+      >
         <span className="text-slate-300">Name:</span>
         <span className="text-lg">Iliya</span>
-      </div>
+      </motion.div>
 
-      <div className="flex flex-col">
+      <motion.div
+        initial={{ opacity: 0, x: "100px" }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, ease: "easeInOut", delay: 0.1 }}
+        className="flex flex-col"
+      >
         <span className="text-slate-300">Date of birth:</span>
         <span className="text-lg">2008/5/9</span>
-      </div>
+      </motion.div>
 
-      <div className="flex flex-col">
+      <motion.div
+        initial={{ opacity: 0, x: "100px" }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, ease: "easeInOut", delay: 0.2 }}
+        className="flex flex-col"
+      >
         <span className="text-slate-300">Experience:</span>
         <span className="text-lg">+3 years</span>
-      </div>
+      </motion.div>
 
-      <div className="flex flex-col">
+      <motion.div
+        initial={{ opacity: 0, x: "100px" }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, ease: "easeInOut", delay: 0.3 }}
+        className="flex flex-col"
+      >
         <span className="text-slate-300">Location:</span>
         <span className="text-lg">Iran</span>
-      </div>
+      </motion.div>
     </div>
   );
 };
--- components/home/about/title.tsx
+++ components/home/about/title.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 const Title = () => {
   return (
     <motion.h2
-      initial={{ opacity: 0, y: "100px" }}
+      initial={{ opacity: 0, y: "50px" }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, ease: "easeInOut" }}
       className="text-5xl font-bold leading-snug"

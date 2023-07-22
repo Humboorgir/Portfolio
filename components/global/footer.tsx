d9a52feb2e3667156e86ef4945f2125e2fadc7f4
Humboorgir <hardcraftap@gmail.com>
2023-07-23 00:09:25 +0330

    Rewrite footer for like the third time

--- components/global/footer.tsx
+++ components/global/footer.tsx
@@ -3,39 +3,34 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center justify-center gap-6 border-t border-t-neutral-700 p-8 px-4 md:px-20">
-      {/* logo and copyright statement  */}
-      <div className="flex flex-col items-center text-center">
-        <Logo />
-        <p className="mt-3 text-lg text-slate-200">
-          Iliya fazlollahi <br /> Programming since 2020
-        </p>
-        <p className="mt-2 text-sm text-zinc-400">
-          Copyright &copy; 2023 - All right reserved
-        </p>
-      </div>
-
+    <footer
+      className="relative mt-52 flex flex-col items-center justify-center p-8 px-4 
+      text-center before:absolute before:left-[50%] before:top-[50%] before:-z-10 before:h-full 
+      before:w-full before:translate-x-[-50%] before:translate-y-[-50%]
+       before:rounded-3xl before:bg-gray-800 before:blur-[160px]
+       before:content-[''] after:absolute after:left-[50%] after:top-0
+           after:h-1 after:w-[min(500px,80vw)] after:translate-x-[-50%] after:rounded-full after:bg-gradient-to-r
+            after:from-zinc-600 after:to-zinc-400 md:px-[20%]"
+    >
+      {/* logo and copyright statement  */}{" "}
+      <p className="mt-3 h-[42px] text-4xl font-bold tracking-wider text-slate-200">
+        Iliya
+      </p>
       {/* social media links  */}
-      <div className="flex items-center justify-center gap-2 text-3xl">
-        <FaGithub />
-        <FaDiscord />
-        <FaYoutube />
-        <FaInstagram />
+      <div className="mt-4 flex flex-col gap-2">
+        <div className="flex items-center justify-center gap-2 text-3xl">
+          <FaGithub />
+          <FaDiscord />
+          <FaYoutube />
+          <FaInstagram />
+        </div>
+        <h3 className="font-bold text-slate-300">Helpful links</h3>
       </div>
+      <p className="mt-4 text-sm text-zinc-400">
+        Copyright &copy; 2023 - All right reserved
+      </p>
     </footer>
   );
 };
-
-function Logo() {
-  return (
-    <Image
-      className="rounded-full"
-      height={120}
-      width={120}
-      src="/pfp.png"
-      alt="Iliya"
-    />
-  );
-}
 
 export default Footer;

4cf8047eff97501e29dfeedd3f51c58e8377dec9
Humboorgir <hardcraftap@gmail.com>
2023-07-22 23:33:13 +0330

    Rewrite footer (again)

--- components/global/footer.tsx
+++ components/global/footer.tsx
@@ -3,27 +3,18 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer
-      className="flex flex-col items-start justify-between gap-8 border-t border-t-neutral-700 p-8 px-4 
-    md:flex-row md:items-center md:px-20"
+      className="flex flex-col items-center justify-center gap-8 border-t border-t-neutral-700 p-8 px-4 
+    md:flex-row md:px-20"
     >
       {/* logo and copyright statement  */}
-      <div className="flex items-center gap-3">
+      <div className="flex flex-col items-center text-center">
         <Logo />
-        <div>
-          <h3 className="text-2xl text-slate-200">Iliya</h3>
-          <p className="text-sm text-zinc-300">
-            Copyright © 2023 Humboorgir-portfolio,
-            <br /> All rights reserved.
-          </p>
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 place-items-center gap-1 text-sm">
-        <h3 className="mb-0.5 text-xl text-slate-500">LINKS</h3>
-        <span>Home</span>
-        <span>About</span>
-        <span>Projects</span>
-        <span>Contact</span>
+        <p className="mt-3 text-lg text-slate-200">
+          Iliya fazlollahi <br /> Programming since 2020
+        </p>
+        <p className="mt-2 text-sm text-zinc-400">
+          Copyright &copy; 2023 - All right reserved
+        </p>
       </div>
     </footer>
   );
@@ -33,8 +24,8 @@ function Logo() {
   return (
     <Image
       className="rounded-full"
-      height={80}
-      width={80}
+      height={120}
+      width={120}
       src="/pfp.png"
       alt="Iliya"
     />

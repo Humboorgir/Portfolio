8e56c0c17123fcb661435843bd5f4b0bcf4e211f
Humboorgir <hardcraftap@gmail.com>
2023-07-22 22:57:07 +0330

    Rewrite footer (display copyright statement)

--- components/global/footer.tsx
+++ components/global/footer.tsx
@@ -1,14 +1,34 @@
+import Image from "next/image";
+
 const Footer = () => {
   return (
     <footer
       className="flex flex-col items-start justify-between gap-8 border-t-2 border-t-neutral-700 p-8 px-4 
-    md:flex-row md:items-center md:px-[20%]"
+    md:flex-row md:items-center md:px-20"
     >
-      <span>my-email@gmail.com</span>
-      <span>Iliya Copyright &copy; 2023, all rights reserved</span>
-      <span>Github</span>
+      <div className="flex items-center gap-3">
+        <Logo />
+        <div>
+          <h3 className="text-2xl text-slate-200">Iliya</h3>
+          <p className="text-sm text-slate-400">
+            Copyright © 2023 Humboorgir-portfolio, All rights reserved.
+          </p>
+        </div>
+      </div>
     </footer>
   );
 };
+
+function Logo() {
+  return (
+    <Image
+      className="rounded-full"
+      height={80}
+      width={80}
+      src="/pfp.png"
+      alt="Iliya"
+    />
+  );
+}
 
 export default Footer;

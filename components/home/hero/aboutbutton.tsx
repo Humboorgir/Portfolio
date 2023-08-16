de85ae37a7c2b5efb96ba2b5446a92c76955bba8
Humboorgir <hardcraftap@gmail.com>
2023-08-17 00:33:23 +0330

    Give hero section's elements a min width and increase its title's font size

--- components/home/hero/aboutbutton.tsx
+++ components/home/hero/aboutbutton.tsx
@@ -5,7 +5,7 @@ const AboutButton = () => {
     <Button
       variant="blue"
       href="#about"
-      className="!w-full border-2 border-sky-600 bg-none text-sky-400 lg:!w-[auto]"
+      className="!w-full min-w-[260px] border-2 border-sky-600 bg-none text-sky-400 lg:!w-[auto] lg:min-w-[auto]"
     >
       Get to know me better
     </Button>
--- components/home/hero/contactbutton.tsx
+++ components/home/hero/contactbutton.tsx
@@ -2,7 +2,11 @@ import Button from "@/components/global/button";
 
 const ContactButton = () => {
   return (
-    <Button variant="blue" href="#contact" className="!w-full lg:!w-[auto]">
+    <Button
+      variant="blue"
+      href="#contact"
+      className="!w-full min-w-[260px] lg:!w-[auto] lg:min-w-[auto]"
+    >
       Contact me
     </Button>
   );

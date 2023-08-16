2cf8868758533f549bdefb4d7fcd3504f4b3d88a
Humboorgir <hardcraftap@gmail.com>
2023-08-16 22:44:39 +0330

    Use rem instead of vw for hero section's title

--- components/home/hero/title.tsx
+++ components/home/hero/title.tsx
@@ -1,6 +1,6 @@
 const Title = () => {
   return (
-    <h3 className="mb-4 w-full text-[9.5vw] font-bold leading-[10.5vw]  text-white sm:text-6xl lg:w-fit lg:max-w-sm">
+    <h3 className="mb-4 w-full text-5xl font-bold text-white sm:text-6xl lg:w-fit lg:max-w-sm">
       Turning ideas into reality
     </h3>
   );

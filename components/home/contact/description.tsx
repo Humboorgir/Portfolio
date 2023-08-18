36cfdc85993ec16beefa1d96d26784fc2c95fc3c
Humboorgir <hardcraftap@gmail.com>
2023-08-18 22:04:08 +0330

    Create ContactDescription component (components/home/contact/description)

---
+++ components/home/contact/description.tsx
@@ -0,0 +1,9 @@
+const Description = () => {
+  return (
+    <p className="mb-6 ml-3 text-lg text-neutral-400">
+      Let's talk! Send me an email to discuss further
+    </p>
+  );
+};
+
+export default Description;

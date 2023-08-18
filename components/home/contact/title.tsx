7d953b0c34016bfb5df4e0bffb1518fa1074098e
Humboorgir <hardcraftap@gmail.com>
2023-08-18 22:03:48 +0330

    Create ContactTitle component (components/home/contact/title)

--- components/home/contact/title.tsx
+++ components/home/contact/title.tsx
@@ -1,6 +1,6 @@
 const Title = () => {
   return (
-    <h2 className="mb-4 ml-3 mr-auto mt-1 text-3xl font-bold">Contact me</h2>
+    <h2 className="ml-3 text-3xl font-bold">Interested in working together?</h2>
   );
 };
 

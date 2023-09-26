8df4b62b9afe1b6e9e3e05c0596c0acc4feb3f04
Humboorgir <hardcraftap@gmail.com>
2023-09-26 18:58:40 +0330

    Increase spacing under Contact title

--- components/home/contact/title.tsx
+++ components/home/contact/title.tsx
@@ -7,7 +7,7 @@ const Title = () => {
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 1, ease: "easeInOut" }}
       viewport={{ once: true }}
-      className="ml-3 text-xl md:text-3xl"
+      className="mb-1 ml-3 text-xl md:text-3xl"
     >
       Interested in working together?
     </motion.h2>

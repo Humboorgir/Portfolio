02357c1a26aa0473db16183cbec3e264f1e3d734
Humboorgir <hardcraftap@gmail.com>
2023-09-26 18:53:36 +0330

    Prevent Hero title from being displayed in 3 lines on small screens

--- components/home/hero/title.tsx
+++ components/home/hero/title.tsx
@@ -1,6 +1,6 @@
 const Title = () => {
   return (
-    <h3 className="mb-4 w-full min-w-[260px] text-[2.7rem] font-bold leading-[2.7rem] text-white sm:text-6xl lg:w-fit lg:max-w-sm">
+    <h3 className="mb-4 min-w-[266px] text-[2.7rem] font-bold leading-[2.7rem] text-white sm:text-6xl lg:w-fit lg:max-w-sm">
       Turning ideas into reality
     </h3>
   );
